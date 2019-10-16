38dfdb1c1efb2db6a05ed4cb6560c6e380b84d66
AkinaSakiryan <471719490@qq.com>
2019-10-16 10:08:50 +0800

    test

---
+++ code-snippet/src/main/java/com/yzl/snippet/CodeSnippetApplication.java
@@ -0,0 +1,13 @@
+package com.yzl.snippet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnippetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSnippetApplication.class, args);
+    }
+
+}
